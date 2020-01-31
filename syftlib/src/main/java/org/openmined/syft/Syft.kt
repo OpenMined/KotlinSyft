77e7c6fa9b2b2194cc668db3f17cf9be6ad6a0c5
varun khare <varunkhare1234@gmail.com>
2020-01-31 15:40:01 +0100

    Fixing style guidelines (#25)
    * added project style file
    
    * reformat code

--- syftlib/src/main/java/org/openmined/syft/Syft.kt
+++ syftlib/src/main/java/org/openmined/syft/Syft.kt
@@ -2,8 +2,8 @@ package org.openmined.syft
 
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.serialization.ImplicitReflectionSerializer
-import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
 import org.openmined.syft.network.NetworkMessage
 import org.openmined.syft.network.SignallingClient
 import org.openmined.syft.threading.ProcessSchedulers
@@ -28,33 +28,35 @@ class Syft(
         )
 
         val disposable = signallingClient.start()
-            .map {
-                // TODO Please excuse this terrible piece of code
-                when (it) {
-                    is NetworkMessage.SocketOpen -> {
-                        println("Socket open")
-                        send("And now that we are opened, I send a message")
+                .map {
+                    // TODO Please excuse this terrible piece of code
+                    when (it) {
+                        is NetworkMessage.SocketOpen -> {
+                            println("Socket open")
+                            send("And now that we are opened, I send a message")
+                        }
+                        is NetworkMessage.SocketClosed -> println("Socket was closed successfully")
+                        is NetworkMessage.SocketError -> println(it.throwable.message)
+                        is NetworkMessage.MessageReceived -> println(it)
+                        is NetworkMessage.MessageSent -> println("Message sent successfully")
                     }
-                    is NetworkMessage.SocketClosed -> println("Socket was closed successfully")
-                    is NetworkMessage.SocketError -> println(it.throwable.message)
-                    is NetworkMessage.MessageReceived -> println(it)
-                    is NetworkMessage.MessageSent -> println("Message sent successfully")
                 }
-            }
-            .subscribeOn(schedulers.computeThreadScheduler)
-            .observeOn(schedulers.calleeThreadScheduler)
-            .subscribe()
+                .subscribeOn(schedulers.computeThreadScheduler)
+                .observeOn(schedulers.calleeThreadScheduler)
+                .subscribe()
 
         compositeDisposable.add(disposable)
     }
 
     @ImplicitReflectionSerializer
     fun send(message: String) {
-        signallingClient.send("Personal", JsonObject(
-            mapOf(
-                "data" to Json.plain.toJson(message),
-                "workerId" to Json.plain.toJson("1234")
+        signallingClient.send(
+            "Personal", JsonObject(
+                mapOf(
+                    "data" to JsonPrimitive(message),
+                    "workerId" to JsonPrimitive("1234")
+                )
             )
-        ))
+        )
     }
 }
