ec37900e6050e51b434add857bfc57c5a4154a36
Varun Khare <varunkhare1234@gmail.com>
2020-03-03 21:19:33 +0100

    updated standalone-demo

--- demo-app/src/main/java/org/openmined/syft/demo/StandaloneDemo.kt
+++ demo-app/src/main/java/org/openmined/syft/demo/StandaloneDemo.kt
@@ -3,21 +3,23 @@ package org.openmined.syft.demo
 import io.reactivex.Scheduler
 import io.reactivex.schedulers.Schedulers
 import org.openmined.syft.Syft
-import org.openmined.syft.networking.clients.SyftWebSocket
-import org.openmined.syft.networking.requests.Protocol
+import org.openmined.syft.networking.clients.HttpClient
+import org.openmined.syft.networking.clients.SocketClient
 import org.openmined.syft.threading.ProcessSchedulers
 
 @ExperimentalUnsignedTypes
 fun main() {
-    val syft = Syft.getInstance(SyftWebSocket(
-        Protocol.WSS,
-        "echo.websocket.org",
-        2000u
-    ), object : ProcessSchedulers {
+    val schedulers = object : ProcessSchedulers {
         override val computeThreadScheduler: Scheduler
             get() = Schedulers.computation()
         override val calleeThreadScheduler: Scheduler
             get() = Schedulers.single()
     }
+    val syft = Syft.getInstance(
+        SocketClient(
+            "echo.websocket.org",
+            2000u
+            , schedulers
+        ), HttpClient("echo.websocket.org"), schedulers
     )
 }
