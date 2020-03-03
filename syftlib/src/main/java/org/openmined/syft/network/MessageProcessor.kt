bfd04068de2940b6fa83c4687db7182972e739e5
mccorby <mccorby@gmail.com>
2020-03-03 14:59:11 +0000

    Unbuffer plan operations

--- syftlib/src/main/java/org/openmined/syft/network/MessageProcessor.kt
+++ syftlib/src/main/java/org/openmined/syft/network/MessageProcessor.kt
@@ -1,5 +1,10 @@
-package org.openmined.syft.network
+package org.openmined.syft.networking.clients
+
+import org.openmined.syftproto.execution.v1.PlanOuterClass
 
 class MessageProcessor {
 
+    fun processPlan(byteArray: ByteArray): PlanOuterClass.Plan {
+        return PlanOuterClass.Plan.parseFrom(byteArray)
+    }
 }
