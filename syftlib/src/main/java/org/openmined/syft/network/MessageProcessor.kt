d8ea7844f5996d974709ea5bf5e5e732bdb79d5a
mccorby <mccorby@gmail.com>
2020-03-12 13:43:57 +0000

    Wire up MNIST

--- syftlib/src/main/java/org/openmined/syft/network/MessageProcessor.kt
+++ syftlib/src/main/java/org/openmined/syft/network/MessageProcessor.kt
@@ -1,10 +1,16 @@
 package org.openmined.syft.networking.clients
 
+import org.openmined.syftproto.execution.v1.PlanOuterClass
 import org.openmined.syftproto.types.torch.v1.ScriptModuleOuterClass
 
 class MessageProcessor {
 
-    fun processScriptModule(byteArray: ByteArray): ScriptModuleOuterClass.ScriptModule {
+    fun processTorchScript(byteArray: ByteArray): ScriptModuleOuterClass.ScriptModule {
         return ScriptModuleOuterClass.ScriptModule.parseFrom(byteArray)
+    }
+
+    fun processPlan(byteArray: ByteArray): PlanOuterClass.Plan {
+
+        return PlanOuterClass.Plan.parseFrom(byteArray)
     }
 }
--- syftlib/src/test/java/org/openmined/syft/networking/clients/MessageProcessorTest.kt
+++ syftlib/src/test/java/org/openmined/syft/networking/clients/MessageProcessorTest.kt
@@ -5,14 +5,27 @@ import org.junit.jupiter.api.Test
 internal class MessageProcessorTest {
 
     @Test
-    fun `Given a serialized plan then message processor deserializes it`() {
+    fun `Given a serialized torchscript then message processor deserializes it`() {
         val planInputStream = javaClass.classLoader?.getResourceAsStream("proto_files/tp_ts.pb")
 
         val planPb = planInputStream?.buffered().use { it!!.readBytes() }
 
         val cut = MessageProcessor()
 
-        val result = cut.processScriptModule(planPb)
+        val result = cut.processTorchScript(planPb)
+
+        assert(result.isInitialized)
+    }
+
+    @Test
+    fun `Given a serialized plan then message processor deserializes it`() {
+        val planInputStream = javaClass.classLoader?.getResourceAsStream("proto_files/tp_ops.pb")
+
+        val planPb = planInputStream?.buffered().use { it!!.readBytes() }
+
+        val cut = MessageProcessor()
+
+        val result = cut.processPlan(planPb)
 
         assert(result.isInitialized)
     }
