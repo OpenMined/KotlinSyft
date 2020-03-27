d8547672d47022562079bbea9075201b64e6a63e
Varun Khare <varunkhare1234@gmail.com>
2020-03-27 12:07:02 +0100

    updated demo app to use new syft worker

--- demo-app/src/main/java/org/openmined/syft/demo/ui/FederatedCycleViewModel.kt
+++ demo-app/src/main/java/org/openmined/syft/demo/ui/FederatedCycleViewModel.kt
@@ -13,13 +13,13 @@ private const val TAG = "FederatedCycleViewModel"
 
 @ExperimentalUnsignedTypes
 class FederatedCycleViewModel(
-    socketClient: SocketClient,
-    httpClient: HttpClient,
+    baseurl: String,
+    authToken: String,
     networkSchedulers: ProcessSchedulers,
     computeSchedulers: ProcessSchedulers
 ) : ViewModel() {
     private val syftWorker = Syft.getInstance(
-        socketClient, httpClient,
+        baseurl, authToken,
         networkSchedulers, computeSchedulers
     )
     private val mnistJob = syftWorker.newJob("mnist", "1.0.0")
