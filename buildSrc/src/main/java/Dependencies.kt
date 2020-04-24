34632b79f3c70ce45cf574a7bc45c4b4de452d15
varun khare <varunkhare1234@gmail.com>
2020-04-24 14:57:31 +0530

    updated syft-proto (#77)
    * updated syft-proto
    
    * removed syft-proto from jetifier

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -25,7 +25,7 @@ object Versions {
     const val webrtc = "1.0.30039"
     const val okhttp = "4.3.1"
     const val protobuf = "3.11.0"
-    const val syftProto = "0.0.8"
+    const val syftProto = "0.0.9"
     const val retrofit = "2.7.1"
     const val kotlinConverter = "0.4.0"
     const val pytorchAndroid = "1.4.0"
