9227857be4b6dc4e1da56150dfc5c27d052fa239
Ravikant Singh <ravikantsingh2308@gmail.com>
2020-06-12 17:43:31 +0530

    Docs HttpAPI interface (#192)
    * Docs HttpAPI
    
    * update
    
    * update

--- syftlib/src/main/java/org/openmined/syft/networking/requests/HttpAPI.kt
+++ syftlib/src/main/java/org/openmined/syft/networking/requests/HttpAPI.kt
@@ -23,8 +23,21 @@ import retrofit2.http.Part
 import retrofit2.http.Query
 import retrofit2.http.Streaming
 
+/**
+ * HttpAPI interface is used to implement http API service to PyGrid Server.
+ *
+ * @See org.openmined.syft.networking.clients.HttpClient
+ */
 interface HttpAPI : CommunicationAPI {
 
+    /**
+     * Check connection speed by ping to PyGrid Server.
+     *
+     * @param isPing Allow PyGrid to differentiate between CheckPing request vs DownloadSpeedTest request.
+     * In case of CheckPing it is always true.
+     * @param workerId Id of syft worker handling this job.
+     * @param random A random integer bit stream.
+     */
     @GET("federated/speed-test")
     fun checkPing(
         @Query("is_ping") isPing: Int = 1,
@@ -32,6 +45,12 @@ interface HttpAPI : CommunicationAPI {
         @Query("random") random: String
     ): Single<Response<SpeedCheckResponse>>
 
+    /**
+     * Check download speed from PyGrid Server.
+     *
+     * @param workerId Id of syft worker handling this job.
+     * @param random A random integer bit stream.
+     */
     @Streaming
     @GET("federated/speed-test")
     fun downloadSpeedTest(
@@ -39,6 +58,14 @@ interface HttpAPI : CommunicationAPI {
         @Query("random") random: String
     ): Single<Response<ResponseBody>>
 
+    /**
+     * Check upload speed to PyGrid Server by uploading a file using multipart post request.
+     *
+     * @param workerId Id of syft worker handling this job.
+     * @param random A random integer bit stream.
+     * @param description Meta-data for upload process.
+     * @param file_body A file to be uploaded to check upload speed.
+     */
     @Multipart
     @POST("federated/speed-test")
     fun uploadSpeedTest(
@@ -48,6 +75,15 @@ interface HttpAPI : CommunicationAPI {
         @Part file_body: MultipartBody.Part
     ): Single<Response<SpeedCheckResponse>>
 
+    /**
+     * Download Plans from PyGrid Server.
+     *
+     * @param workerId Id of syft worker handling this job.
+     * @param requestKey A unique key required for authorised communication with PyGrid server.
+     * It ensures that only workers accepted for a cycle can receive Plan data from server.
+     * @param planId Id of the Plan to be downloaded.
+     * @param op_type Format in which Plan operations are defined, Can be torchScript.
+     */
     //    @Streaming
     @GET("/federated/get-plan")
     fun downloadPlan(
@@ -57,6 +93,14 @@ interface HttpAPI : CommunicationAPI {
         @Query("receive_operations_as") op_type: String
     ): Single<Response<ResponseBody>>
 
+    /**
+     * Downloads Protocols from PyGrid Server.
+     *
+     * @param workerId Id of syft worker handling this job.
+     * @param requestKey A unique key required for authorised communication with PyGrid server.
+     * It ensures that only workers accepted for a cycle can receive Protocol data from server.
+     * @param protocolId Id of the Protocol to be downloaded.
+     */
     //    @Streaming
     @GET("/federated/get-protocol")
     fun downloadProtocol(
@@ -65,6 +109,14 @@ interface HttpAPI : CommunicationAPI {
         @Query("protocol_id") protocolId: String
     ): Single<Response<ResponseBody>>
 
+    /**
+     * Download Model from PyGrid Server.
+     *
+     * @param workerId Id of syft worker handling this job.
+     * @param requestKey A unique key required for authorised communication with PyGrid server.
+     * It ensures that only workers accepted for a cycle can receive Model data from server.
+     * @param modelId Id of the model to be downloaded.
+     */
     //    @Streaming
     @GET("/federated/get-model")
     fun downloadModel(
@@ -73,12 +125,30 @@ interface HttpAPI : CommunicationAPI {
         @Query("model_id") modelId: String
     ): Single<Response<ResponseBody>>
 
+    /**
+     * Calls **federated/authenticate** for authentication.
+     *
+     * @param authRequest Contains JWT auth-token. JWT authentication protects the model from sybil attacks.
+     */
     @GET(AUTH_TYPE)
     override fun authenticate(authRequest: AuthenticationRequest): Single<AuthenticationResponse>
 
+    /**
+     * Calls **federated/cycle-request** for requesting PyGrid server for training cycle.
+     * Response of server can be CycleAccept or CycleReject
+     * @see CycleResponseData.CycleAccept
+     * @see CycleResponseData.CycleReject
+     *
+     * @param cycleRequest @see org.openmined.syft.networking.datamodels.syft.CycleRequest
+     */
     @POST(CYCLE_TYPE)
     override fun getCycle(@Body cycleRequest: CycleRequest): Single<CycleResponseData>
 
+    /**
+     * Calls **federated/report** for sending the updated model back to PyGrid.
+     *
+     * @param reportRequest Contains worker-id and request-key.
+     */
     @POST(REPORT_TYPE)
     override fun report(@Body reportRequest: ReportRequest): Single<ReportResponse>
 }
