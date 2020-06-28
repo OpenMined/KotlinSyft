c1ba98bbcac2a9f636a71e2d31f5e68ea2b32be6
mccorby <mccorby@gmail.com>
2020-06-28 14:22:08 +0100

    Remove that file

--- syftlib/ForYourEyesOnly.ws.kts
+++
@@ -1,9 +0,0 @@
-import java.io.File
-
-val file = File("/tmp/whatisthis/thisismyfile.txt")
-file.parentFile?.let {
-    if (!it.exists()) it.mkdirs()
-} ?:
-file.useLines{"Content"})
-
-println(file.absolutePath)
