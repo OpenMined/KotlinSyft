45fc9bcbba587fe58e737deb92eb4a095d933085
mccorby <mccorby@gmail.com>
2020-06-28 18:00:44 +0100

    Cover content types

--- syftlib/src/test/java/org/openmined/syft/proto/SyftTensorTest.kt
+++ syftlib/src/test/java/org/openmined/syft/proto/SyftTensorTest.kt
@@ -1,6 +1,5 @@
 package org.openmined.syft.proto
 
-import com.nhaarman.mockitokotlin2.any
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
@@ -8,7 +7,6 @@ import io.mockk.mockk
 import org.junit.Before
 import org.junit.Test
 import org.openmined.syftproto.types.syft.v1.IdOuterClass
-import org.openmined.syftproto.types.torch.v1.SizeOuterClass
 import org.openmined.syftproto.types.torch.v1.Tensor
 import org.openmined.syftproto.types.torch.v1.TensorDataOuterClass
 import org.pytorch.DType
@@ -52,7 +50,7 @@ class SyftTensorTest {
     }
 
     @Test
-    fun `Given a TorchTensor when transform into SyftTensor then returns the expected object`() {
+    fun `Given a TorchTensor with int32 contents when transform into SyftTensor then returns the expected object`() {
         val shape = listOf(1, 2)
         val torchTensor = mockk<TorchTensor>() {
             every { shape() } returns longArrayOf(1, 2)
@@ -65,6 +63,60 @@ class SyftTensorTest {
         assert(shape == result.shape)
         assert("int32" == result.dtype)
         assert(listOf(100, 100, 100) == result.contents.contentsInt32List)
+        assert(result.tags.isEmpty())
+        assert(result.description.isEmpty())
+    }
+
+    @Test
+    fun `Given a TorchTensor with float32 contents when transform into SyftTensor then returns the expected object`() {
+        val shape = listOf(1, 2)
+        val torchTensor = mockk<TorchTensor>() {
+            every { shape() } returns longArrayOf(1, 2)
+            every { dtype() } returns DType.FLOAT32
+            every { dataAsFloatArray } returns floatArrayOf(100F, 100F, 100F)
+        }
+
+        val result = torchTensor.toSyftTensor()
+
+        assert(shape == result.shape)
+        assert("float32" == result.dtype)
+        assert(listOf(100F, 100F, 100F) == result.contents.contentsFloat32List)
+        assert(result.tags.isEmpty())
+        assert(result.description.isEmpty())
+    }
+
+    @Test
+    fun `Given a TorchTensor with int64 contents when transform into SyftTensor then returns the expected object`() {
+        val shape = listOf(1, 2)
+        val torchTensor = mockk<TorchTensor>() {
+            every { shape() } returns longArrayOf(1, 2)
+            every { dtype() } returns DType.INT64
+            every { dataAsLongArray } returns longArrayOf(100, 100, 100)
+        }
+
+        val result = torchTensor.toSyftTensor()
+
+        assert(shape == result.shape)
+        assert("int64" == result.dtype)
+        assert(listOf(100L, 100L, 100L) == result.contents.contentsInt64List)
+        assert(result.tags.isEmpty())
+        assert(result.description.isEmpty())
+    }
+
+    @Test
+    fun `Given a TorchTensor with float64 contents when transform into SyftTensor then returns the expected object`() {
+        val shape = listOf(1, 2)
+        val torchTensor = mockk<TorchTensor>() {
+            every { shape() } returns longArrayOf(1, 2)
+            every { dtype() } returns DType.FLOAT64
+            every { dataAsDoubleArray } returns doubleArrayOf(100.0, 100.0, 100.0)
+        }
+
+        val result = torchTensor.toSyftTensor()
+
+        assert(shape == result.shape)
+        assert("float64" == result.dtype)
+        assert(listOf(100.0, 100.0, 100.0) == result.contents.contentsFloat64List)
         assert(result.tags.isEmpty())
         assert(result.description.isEmpty())
     }
