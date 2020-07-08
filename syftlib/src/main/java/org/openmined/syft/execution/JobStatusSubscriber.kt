cb13fe9cf5bba4a87560f2937e7c00dd52d47716
Varun Khare <varunkhare1234@gmail.com>
2020-07-08 17:13:32 +0530

    add description of the class

--- syftlib/src/main/java/org/openmined/syft/execution/JobStatusSubscriber.kt
+++ syftlib/src/main/java/org/openmined/syft/execution/JobStatusSubscriber.kt
@@ -5,6 +5,27 @@ import org.openmined.syft.proto.SyftModel
 import java.io.InvalidObjectException
 import java.util.concurrent.ConcurrentHashMap
 
+/**
+ * This is passed as argument to [SyftJob.start] giving the overridden callbacks for different phases of the job cycle.
+ * ```kotlin
+ * val jobStatusSubscriber = object : JobStatusSubscriber() {
+ *      override fun onReady(
+ *      model: SyftModel,
+ *      plans: ConcurrentHashMap<String, Plan>,
+ *      clientConfig: ClientConfig
+ *      ) {
+ *      }
+ *
+ *      override fun onRejected(timeout: String) {
+ *      }
+ *
+ *      override fun onError(throwable: Throwable) {
+ *      }
+ * }
+ *
+ * job.start(jobStatusSubscriber)
+ * ```
+ */
 @ExperimentalUnsignedTypes
 open class JobStatusSubscriber {
     /**
