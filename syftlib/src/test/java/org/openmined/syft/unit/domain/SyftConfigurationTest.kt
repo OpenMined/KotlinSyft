e37d59ea5232c00c480509e2cd6d21e3d9743912
Varun Khare <varunkhare1234@gmail.com>
2020-07-08 17:44:05 +0530

    adding tests for codecov

--- syftlib/src/test/java/org/openmined/syft/unit/domain/SyftConfigurationTest.kt
+++ syftlib/src/test/java/org/openmined/syft/unit/domain/SyftConfigurationTest.kt
@@ -1,6 +1,7 @@
 package org.openmined.syft.unit.domain
 
 import android.content.Context
+import android.net.NetworkCapabilities
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
@@ -34,5 +35,21 @@ class SyftConfigurationTest {
                 .enableBackgroundServiceExecution()
                 .build()
         assert(!config.monitorDevice)
+    }
+
+    @Test
+    fun `When enableCellularData is called SyftConfiguration has networkTransportMedium is set to Cellular`() {
+        val config = SyftConfiguration.builder(context, "test")
+                .enableCellularData()
+                .build()
+        assert(config.transportMedium == NetworkCapabilities.TRANSPORT_CELLULAR)
+    }
+
+    @Test
+    fun `When enableMeteredData is called SyftConfiguration has networkConstraints set to unmetered`() {
+        val config = SyftConfiguration.builder(context, "test")
+                .enableMeteredData()
+                .build()
+        assert(!config.networkConstraints.contains(NetworkCapabilities.NET_CAPABILITY_NOT_METERED))
     }
 }
