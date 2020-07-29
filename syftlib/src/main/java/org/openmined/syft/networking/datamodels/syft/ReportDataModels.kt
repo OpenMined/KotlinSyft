1b820ee4158d62ca1d13ea7271179a0256310336
Patrick Cason <me@patrickcason.com>
2020-07-29 19:36:22 +0100

    Namespacing changes and changes from static to model centric

--- syftlib/src/main/java/org/openmined/syft/networking/datamodels/syft/ReportDataModels.kt
+++ syftlib/src/main/java/org/openmined/syft/networking/datamodels/syft/ReportDataModels.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.openmined.syft.networking.datamodels.NetworkModels
 
-internal const val REPORT_TYPE = "model_centric/report"
+internal const val REPORT_TYPE = "model-centric/report"
 
 @Serializable
 internal data class ReportResponse(
