b95d4d5ea44719fe20e70d642a19587c3c1e3e6d
Mohammed Galalen <mohammedgalalen@gmail.com>
2020-10-27 11:49:10 +0200

    complete unit tests for battery repo

--- syft/src/test/java/org/openmined/syft/unit/monitor/battery/BatteryStatusRepositoryTest.kt
+++ syft/src/test/java/org/openmined/syft/unit/monitor/battery/BatteryStatusRepositoryTest.kt
@@ -1,6 +1,7 @@
 package org.openmined.syft.unit.monitor.battery
 
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
 import org.junit.Test
 import org.openmined.syft.monitor.battery.BatteryStatusDataSource
@@ -9,13 +10,40 @@ import org.openmined.syft.monitor.battery.BatteryStatusRepository
 @ExperimentalUnsignedTypes
 class BatteryStatusRepositoryTest {
 
-    private val repository = BatteryStatusRepository(mock())
-    private val batteryStatusDataSource = mock<BatteryStatusDataSource>()
+    private val batteryLevel = 100f
+    private val batteryStatusDataSource = mock<BatteryStatusDataSource> {
+        on { getBatteryLevel() }.thenReturn(batteryLevel)
+        on { getBatteryValidity() }.thenReturn(true)
+        on { checkIfCharging() }.thenReturn(false)
+    }
+
+    private val repository = BatteryStatusRepository(batteryStatusDataSource)
 
     @Test
     fun `verify calling repository getBatteryValidity will call dataSource getBatteryValidity`() {
-        repository.getBatteryValidity()
-        verify(batteryStatusDataSource).getBatteryValidity()
+        val validity = repository.getBatteryValidity()
+        assert(validity)
+        verify(batteryStatusDataSource, times(1)).getBatteryValidity()
+    }
+
+    @Test
+    fun `verify subscribe to state change will call subscribe on dataSource`() {
+        repository.subscribeStateChange()
+        verify(batteryStatusDataSource, times(1)).subscribeStateChange()
+    }
+
+    @Test
+    fun `verify unsubscribe from state change will call unsubscribe on dataSource`() {
+        repository.unsubscribeStateChange()
+        verify(batteryStatusDataSource, times(1)).unsubscribeStateChange()
+    }
+
+    @Test
+    fun `verify get battery state will return battery status model`() {
+        val model = repository.getBatteryState()
+        assert(!model.isCharging)
+        assert(model.batteryLevel == batteryLevel)
+        assert(model.cacheTimeStamp > 0)
     }
 
 }
