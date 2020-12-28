b1a92448c33468258d7f4a7c86a0c1d61bae2ac5
Mohammed Galalen <mohammedgalalen@gmail.com>
2020-12-28 13:27:52 +0200

    add more unit tests

--- syft/src/main/java/org/openmined/syft/data/Util.kt
+++
@@ -1,5 +0,0 @@
-package org.openmined.syft.data
-
-object Util {
-
-}
--- syft/src/test/java/org/openmined/syft/unit/data/DataLoaderTest.kt
+++ syft/src/test/java/org/openmined/syft/unit/data/DataLoaderTest.kt
@@ -3,6 +3,7 @@ package org.openmined.syft.unit.data
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import org.junit.Test
+import org.openmined.syft.data.BaseDataLoaderIterator
 import org.openmined.syft.data.DataLoader
 import org.openmined.syft.data.Dataset
 import org.openmined.syft.data.KTensor
@@ -20,15 +21,16 @@ class DataLoaderTest {
         on { length() }.thenReturn(10)
     }
 
-    private val dataLoader = DataLoader(dataset, 3)
-
     @Test
     fun `indexSampler returns sequential indices when shuffle is false`() {
+        val dataLoader = DataLoader(dataset, batchSize = 3, shuffle = false)
         assert(dataLoader.indexSampler().indices() == listOf(0, 1, 2))
     }
 
     @Test
     fun `indexSampler returns random indices when shuffle is true`() {
+        val dataLoader = DataLoader(dataset, batchSize = 3, shuffle = true)
+
         val indices = (0 until dataset.length())
         dataLoader.indexSampler().indices().forEach {
             assert(it in indices)
@@ -38,6 +40,7 @@ class DataLoaderTest {
     @Test
     fun `when dropLast is true indexSampler should drop batches not equal to batchSize`() {
         val dataLoader = DataLoader(dataset, batchSize = 3, dropLast = true)
+
         assert(dataLoader.indexSampler().indices().size == 3)
         assert(dataLoader.indexSampler().indices().size == 3)
         assert(dataLoader.indexSampler().indices().size == 3)
@@ -47,10 +50,33 @@ class DataLoaderTest {
     @Test
     fun `when dropLast is false indexSampler should returns all data in batches`() {
         val dataLoader = DataLoader(dataset, batchSize = 3, dropLast = false)
+
         assert(dataLoader.indexSampler().indices().size == 3)
         assert(dataLoader.indexSampler().indices().size == 3)
         assert(dataLoader.indexSampler().indices().size == 3)
         assert(dataLoader.indexSampler().indices().size == 1)
+    }
+
+    @Test
+    fun `dataLoader iterator consumes data correctly`() {
+        val dataLoader = DataLoader(dataset, batchSize = 3)
+
+        val iterator = dataLoader.iterator()
+        assert(iterator.hasNext())
+        for (i in 0 until dataLoader.indexSampler().length())
+            iterator.next()
+        assert(!iterator.hasNext())
+    }
+
+    @Test
+    fun `BaseIterator consumes data correctly`() {
+        val dataLoader = DataLoader(dataset, batchSize = 3)
+
+        val iterator = BaseDataLoaderIterator(dataLoader)
+        assert(iterator.hasNext())
+        for (i in 0 until dataLoader.indexSampler().length())
+            iterator.next()
+        assert(!iterator.hasNext())
     }
 
 }
